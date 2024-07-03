715c0e6595b589a8035c496ee4cb70348f3dc9ff
unknown <Jlassi-Saiida>
2024-07-03 18:04:39 +0200

    Ajout de fonctionnalité XYZ dans ma-nouvelle-branche

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+
+	private final WebDriver driver;
+	WebDriverWait wait;
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	}
+
+	// Les localisateurs
+	private final By usernameField = By.id("user-name");
+	private final By passwordField = By.id("password");
+	private final By loginButton = By.id("login-button");
+
+	public void enterUsername(String username) {
+		driver.findElement(usernameField).sendKeys(username);
+	}
+
+	public void enterPassword(String password) {
+		driver.findElement(passwordField).sendKeys(password);
+	}
+
+	public void clickLoginButton() {
+		driver.findElement(loginButton).click();
+	}
+
+	public void login(String username, String password) {
+		enterUsername(username);
+		enterPassword(password);
+		clickLoginButton();
+	}
+
+}
---
+++ src/test/java/pagesTest/LoginPageTest.java
@@ -0,0 +1,20 @@
+package pagesTest;
+
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import setup.BaseTest;
+import utils.DataInjectionIntern;
+import utils.CSVUtils;
+
+public class LoginPageTest extends BaseTest {
+//	@Test(dataProvider = "userData", dataProviderClass = DataInjectionIntern.class)
+//	public void loginWithUserData(String username, String password) {
+//		new LoginPage(driver).login(username, password);
+//	}
+
+	@Test(dataProvider = "csvData", dataProviderClass = CSVUtils.class)
+	public void loginWithCsvData(String username, String password) {
+		new LoginPage(driver).login(username, password);
+	}
+}
